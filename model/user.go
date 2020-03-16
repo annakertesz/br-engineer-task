ecdd67926b7029db0d3c42c92432024384b5c646
Kertesz Anna <kerteszannanak@gmail.com>
2020-03-16 21:18:24 +0100

    have some user functions

--- model/user.go
+++ model/user.go
@@ -1,7 +1,31 @@
 package model
 
 type User struct{
-	UserInfo string
-	Plan Plan //enum
-	Apps []App
+	userInfo string
+	plan Limit //TODO: Plan should be plan
+	apps []App
+}
+
+func NewUser(userInfo string, plan Limit) User {
+	return User{
+		userInfo: userInfo,
+		plan:     plan,
+		apps:     nil,
+	}
+}
+
+func (user *User) AddApp(app App) {
+	user.apps = append(user.apps, app)
+}
+
+func (user *User) GetApps() []App {
+	return user.apps
+}
+
+func (user *User) SetPlan(limit Limit) {
+	user.plan = limit
+}
+
+func (user *User) GetPlan() Limit {
+	return user.plan
 }
