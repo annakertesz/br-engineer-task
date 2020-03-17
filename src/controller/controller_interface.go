b0d896eb042cf3e64e66f23c571ace3e88755fc0
Kertesz Anna <kerteszannanak@gmail.com>
2020-03-17 20:14:52 +0100

    dumb controller implements controller interface

--- src/controller/controller_interface.go
+++ src/controller/controller_interface.go
@@ -1,12 +1,14 @@
 package controller
 
-import "github.com/annakertesz/br-engineer-task/src/model"
+import (
+	"github.com/annakertesz/br-engineer-task/src/model"
+	"time"
+)
 
 type Controller interface {
 	CreateUser(userName string, plan string) model.User
-	CreateApp(userID string, isOpenSource string)
-	ChangeLimits(appID string, concBuild int, buildTime int, buildPerMonth int, teamMembers int) error
-	OptOutLimits(appID string)
-	UsePublicLimits(appID string)
+	CreateApp(userID string, appName string, openSource bool) (model.App, error)
+	ChangeLimits(appID string, concBuild int, buildTime time.Duration, buildPerMonth int, teamMembers int) error
+	UsePrivateLimits(appID string) error
 	GetLimit(appID string) model.Limit
 }
